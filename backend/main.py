3d680e1769beb46439672a42217f5c0f3949098a
Deekshithpatkar <Dkshith2405@gmail.com>
2026-06-13 15:02:48 +0530

    Added embeedding generation pipeline

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 import shutil
 from backend.pdf_utils import extract_text_from_pdf
 from backend.text_chunker import chunk_text
+from backend.embedding_utils import generate_embeddings
 
 app = FastAPI()
 
@@ -20,10 +21,11 @@ async def upload_pdf(file: UploadFile = File(...)):
     extracted_text = extract_text_from_pdf(file_path)
 
     chunks = chunk_text(extracted_text)
-
+    embeddings = generate_embeddings(chunks)
+    
     return {
-        "filename": file.filename,
-        "total_chunks": len(chunks),
-        "first_chunk": chunks[0],
-        "all_chunks": chunks[:3]
-    }
+    "filename": file.filename,
+    "total_chunks": len(chunks),
+    "embedding_dimension": len(embeddings[0]),
+    "first_chunk_preview": chunks[0][:300]
+}
